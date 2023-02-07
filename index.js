a640bb8a19bc04210d6f071c40b36d1aeedf252a
Shaikateeqahmed <shaikateeqahmed892@gmail.com>
2023-02-07 22:20:58 +0530

    portfolio

--- index.js
+++ index.js
@@ -33,3 +33,39 @@ function c2(){
     },1000);
 }
 c2();
+
+let proj3img = document.getElementById("proj3img");
+function c3(){
+    let images = ["./images/project1img.png","./images/proj1img2.png","./images/proj1img3.png","./images/proj1img4.png"];
+    let imgel = document.createElement("img");
+    imgel.src=images[0];
+    proj3img.append(imgel);
+    let i=1;
+    setInterval(function(){
+        if(i===images.length){
+            i=0;
+        }
+        imgel.src=images[i];
+        proj3img.append(imgel);
+        i++;
+    },1000);
+}
+c3();
+
+let proj4img = document.getElementById("proj4img");
+function c4(){
+    let images = ["./images/proj2img1.png","./images/proj2img2.png","./images/proj2img3.png","./images/proj2img2.png"];
+    let imgel = document.createElement("img");
+    imgel.src=images[0];
+    proj4img.append(imgel);
+    let i=1;
+    setInterval(function(){
+        if(i===images.length){
+            i=0;
+        }
+        imgel.src=images[i];
+        proj4img.append(imgel);
+        i++;
+    },1000);
+}
+c4();
