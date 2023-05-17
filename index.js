c58d2d3d185ee326a743ebd9bc9adc344021030d
Shaikateeqahmed <shaikateeqahmed892@gmail.com>
2023-05-17 14:14:11 +0530

    portfolio some changes

--- index.js
+++ index.js
@@ -70,13 +70,41 @@ function c4(){
 }
 c4();
 
-document.getElementById("resume-link-1").addEventListener("click",resume);
-document.getElementById("resume-link-2").addEventListener("click",resume2);
+// document.getElementById("resume-link-1").addEventListener("click",resume);
+// document.getElementById("resume-link-2").addEventListener("click",resume2);
 
-function resume(){
-    window.open("./Shaik_Ateeq_Ahmed_Resume.pdf","_blank");
-}
+// function resume(){
+//     window.open("./Shaik_Ateeq_Ahmed_Resume.pdf","_blank");
+// }
 
-function resume2(){
-    window.open("./Shaik_Ateeq_Ahmed_Resume.pdf","_blank");
+// function resume2(){
+//     window.open("./Shaik_Ateeq_Ahmed_Resume.pdf","_blank");
+// }
+
+const resume_btn1 = document.querySelector("#resume-button-1");
+const resume_btn2 = document.querySelector("#resume-button-2");
+
+// resume_btn1.addEventListener("click", download);
+// resume_btn2.addEventListener("click", download);
+
+// function download() {
+//   window.open(
+//     "https://drive.google.com/file/d/1PVo0XgJQCZwOPfndmpMkW-Wxav3Z7Ql4/view?usp=share_link",
+//     "_blank"
+//   );
+// }
+
+resume_btn1.addEventListener("click", download);
+resume_btn2.addEventListener("click", download);
+
+// resume_btn2.addEventListener("click",()=>{
+//   window.location.assign("https://drive.google.com/file/d/1PVo0XgJQCZwOPfndmpMkW-Wxav3Z7Ql4/view?usp=share_link", "_blank");
+// })
+
+function download(){
+  // window.location.assign("https://drive.google.com/file/d/1PVo0XgJQCZwOPfndmpMkW-Wxav3Z7Ql4/view?usp=share_link");
+  window.open(
+        "https://drive.google.com/file/d/1PVo0XgJQCZwOPfndmpMkW-Wxav3Z7Ql4/view?usp=share_link",
+        "_blank"
+      );
 }
